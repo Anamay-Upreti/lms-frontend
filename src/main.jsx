9590faa91d13643b648a0302b4a4368b2fbfb25b
Anamay-Upreti <122984489+Anamay-Upreti@users.noreply.github.com>
2023-09-11 22:46:46 +0530

    added react-hot-toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//component import
+import App from './App.jsx';
+// css imports
+import './index.css';
+// library import
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+       <App />
+       <Toaster/>
+  </BrowserRouter>
   
-    <App />
   
 );
